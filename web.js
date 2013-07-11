e30fa389be6230f9b7eb5af06abb9a45cde9e426
John Smith <example@stanford.edu>
2013-07-11 07:49:55 +0000

    Added web.js

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString(fs.readFileSync("index.html")));
+  response.send("dog");
 });
 
 var port = process.env.PORT || 5000;
